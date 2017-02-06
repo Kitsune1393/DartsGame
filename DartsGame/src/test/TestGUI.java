b82003b9f831459bf441df67949d9f815dba8fb3
Jhon Alexander Vega <lenovo@YatoJ>
2017-02-05 22:18:38 -0500

    GUI

---
+++ DartsGame/src/test/TestGUI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import GUI.JFramePrincipal;
+
+/**
+ *
+ * @author lenovo
+ */
+public class TestGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFramePrincipal jFramePrincipal = new JFramePrincipal();
+        jFramePrincipal.setVisible(true);
+    }
+    
+}
